571d4286f319456362db84bd1d65a6bc42406af2
Aileen Cacayorin <acacayorin@gmail.com>
2015-09-18 11:42:32 -0700

    add pizzaCost prototype to Pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,33 @@
 function Order(orderName, orderType, phone) {
-  this.orderName = orderName
-  this.orderType = orderType
-  this.phone = phone
-  this.address = ""
-  this.pizzas = []
-};
+  this.orderName = orderName;
+  this.orderType = orderType;
+  this.phone = phone;
+  this.address = "";
+  this.pizzas = [];
+}
 
 function Pizza(pizzaSize, crustType) {
   this.pizzaSize = pizzaSize;
   this.crustType = crustType;
-  this.toppings = []
+  this.toppings = [];
 }
+
+Pizza.prototype.pizzaCost = function() {
+  var cost = 8;
+
+  if (this.pizzaSize === "medium") {
+    cost += 2;
+
+  } if (this.pizzaSize === "large") {
+    cost += 4;
+  }
+
+  if (this.crustType === "gluten-free" || "stuffed") {
+    cost += 1;
+  }
+
+  this.toppings.forEach(function(topping) {
+    cost += .5;
+  });
+  return cost;
+};
--- spec/specs.js
+++ spec/specs.js
@@ -12,9 +12,21 @@ describe('Order', function() {
 describe('Pizza', function() {
   it('creates a pizza with the given specifications', function(){
     var testPizza = new Pizza('small', 'thin');
+    var pizzaToppings = ['mushrooms', 'extra cheese', 'pepperoni'];
+    pizzaToppings.forEach(function(topping) {
+      testPizza.toppings.push(topping);
+    });
     expect(testPizza.pizzaSize).to.equal('small');
     expect(testPizza.crustType).to.equal('thin');
-    expect(testPizza.toppings).to.eql([]);
+    expect(testPizza.toppings).to.eql(['mushrooms', 'extra cheese', 'pepperoni']);
+  });
 
+  it('calculates the cost of a pizza', function(){
+    var testPizza = new Pizza('medium', 'gluten-free');
+    var pizzaToppings = ['mushrooms', 'extra cheese', 'pepperoni'];
+    pizzaToppings.forEach(function(topping) {
+      testPizza.toppings.push(topping);
+    });
+    expect(testPizza.pizzaCost()).to.equal(12.50);
   });
 });
